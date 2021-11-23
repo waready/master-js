51c1ee15398aa019b76d90ca5f1f6746e2fd410e
waready <elbarto119@hotmail.com>
2021-11-23 13:14:56 -0500

    first commit

---
+++ src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'videojuegos',
+    templateUrl: './videojuego.component.html',
+    //styleUrls: ['./app.component.scss']
+  })
+  export class VideojuegoComponent {
+    title = 'angular';
+  }
